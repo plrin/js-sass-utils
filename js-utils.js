4cdb3753ad1cbce0c78ab02d802b5db22cd04cfc
Tan Dang <plrin@users.noreply.github.com>
2017-07-12 18:37:16 +0200

    added js module boilerplate

--- js-utils.js
+++ js-utils.js
@@ -7,3 +7,48 @@ $(document).find("iframe").each(function (i) {
 $(document).find("iframe").each(function (i) {
   this.contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*')
 });
+
+
+// boilerplate for Modul MVVC
+var PokeApp = (function() {
+  'use strict';
+  // placeholder for cached DOM elements and global variables of modul
+  var DOM = {}
+    , globalVariable = null;
+
+  /* =================== private methods ================= */
+  // cache DOM elements
+  function cacheDom() {
+    DOM.$button = $("button");
+  }
+
+  // bind events
+  function bindEvents() {
+    DOM.$button.on("click", _onStartButton);
+  }
+
+  // set element click function
+  function _onStartButton() {
+    doStuff();
+    // do more stuff
+  }
+
+  // functions
+
+  function doStuff() {
+    // do stuff
+  }
+
+
+  /* =================== public methods ================== */
+  // main init method
+  function init() {
+    cacheDom();
+    bindEvents();
+  }
+
+  /* =============== export public methods =============== */
+  return {
+    init: init
+  };
+}());
